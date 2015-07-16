bf21c144aa74fc91a6146f61813a320fa3ffec60
rmejiadelagala-utn <rmejiadelagala.utn@gmail.com>
2015-07-16 08:23:19 -0300

    pensandolo con listas

--- NodoReduce/aparearArchivosDeLaListaArchivosAReducir.c
+++ NodoReduce/aparearArchivosDeLaListaArchivosAReducir.c
@@ -9,12 +9,22 @@
 #include "estructurasNodo.h"
 #include "funcionesNodo.h"
 
+
+t_list* getFirstRowsFromArchivosAReducir(t_archivoAReducir* unArchivoAReducir ){
+	t_list* Lista_reg; //lista de Registros a aparear
+	t_RegistroArch* unRegistroArch;
+	Lista_reg=list_create();
+    if(esLocal(unArchivoAReducir)){
+    	unRegistroArch=getRegistroDeArchivo()
+    }
+}
+
 int aparearArchivosDeLaListaArchivosAReducir(t_list* archivosAReducir){
 	t_list* Lista_reg; //lista de Registros a aparear
 	t_RegistroArch* rowSeleccionado;
 	/*Me conecto a los remotos y obtengo los sockets y los primeros registros,
 	  Para los archivos locales, obtengo los fileDescriptors */
-	Lista_reg=getFirstRowsFromArchivosAReducir(archivosAReducir);
+	Lista_reg=list_map(archivosAReducir,(void*)getFirstRowsFromArchivosAReducir);
 	while( !list_is_empty(Lista_reg)){
 		rowSeleccionado=apareoDeRegistros(Lista_reg);
 		enviarRowAlPipeDelReduce(rowSeleccionado->registro);
